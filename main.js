af4286c413d3accb7a975ce12d06d6bb92e1558a
aloro lee <alorotams@gmail.com>
2024-10-07 15:56:10 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+a = 3           // declaration, statement, assigning the value 3 to the identifier "a"
+b = 5           // variable b is 5
+c = a + b       // expression a+b adding a and b, then assigning to varibale c 
+console.log(c) // passing c into the log method/function of the console object.
+
+
+//F5 fn+5 RUN/PLAY
+
+let firstname = "Lee"; // javaScript's fundamental/primative data types,
+let greeting = "Hello "; // include numbers and strings of characters/letter.
+
+// Concatenate operator - "Chain together" like a train.
+let message = greeting + firstname;
+
+console.log(message); // "Hello Lee"
+
+let z = "I will be back ";
+let y = "Home soon.";
+
+let my = z + y
+
+console.log(my)

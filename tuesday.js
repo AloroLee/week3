2bedf35e1789e1823a96470bde99da9b80e49ed3
aloro lee <alorotams@gmail.com>
2024-10-08 13:54:08 -0400

    js tuesday

---
+++ tuesday.js
@@ -0,0 +1,47 @@
+console.log("TUESDAY")
+// single line comment 
+
+
+/* slash starts a multiline
+wraps a multi line command
+star-slash ends a multiline comments*/
+
+n = 12 // super chill
+let x = 123; //more formal
+
+i = 123 //integer (whole numbers)
+f = 99.999 //fraction, float (floating point decimal)
+
+num = 1.1 //just number
+
+text = "123.45 words" // 14 bytes? strings of characters - data type string
+number = parseFloat(text) // 2 bytes? for the floating point decimal aka Number
+double = number * 2 //now we can do math!
+console.log(double)
+// Fn & F5 - Run/Debug
+
+//parseInt ignore after  the decimals point
+
+d = new Date()// create a new date object (today)
+console.log(d)
+console.log(d.getTime()) //ms since 1/1/1970 00:00:00
+
+counter = 0
+counter++ //increment - bump up by one
+counter++
+console.log(counter) //????
+counter-- //decrement - minus one
+console.log(counter) //???
+
+let level = 0
+level += 5
+level += 5
+level += 5
+console.log(level) //??
+level -= 5
+console.log(level)
+
+console.log(3+5) // addition
+console.log(3-5) // substraction
+console.log(3*5) // miltiplication
+console.log(3/5) // dividion 

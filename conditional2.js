d8d26e798367a1e0e7c96da9dbe8989bf0716031
aloro lee <alorotams@gmail.com>
2024-10-09 13:50:24 -0400

    updated

---
+++ conditional2.js
@@ -0,0 +1,23 @@
+// LOGICAL OPERATORS
+// && AND (all must be true)
+// ll or (either is true)
+// == comparison (are the the same)
+// === strict comparison (compares both value and datatype)
+// != NOT Equal 
+// !== strict 
+// <= less than or equal to
+// >= greater than or equal to
+// > greater than
+// < less than 
+
+height = 60
+minimum = 55
+
+console.log("start") // always runs
+
+if (height >= minimum){ //depends on the condition
+    console.log("weee!!!");
+}else{
+    console.log("watching the fun")
+}
+
---
+++ conditionals.js
@@ -0,0 +1,28 @@
+// COMPUTER VALUES ARE 0 and 1, AKA off and on
+// LOGIC TRUE OR FALSE, 0 is usually false and non-zero is usually true
+
+// Bool - Boolean (true/false) - Reason/Philosophy into math?
+// && combines two boolean
+
+//INPUTS
+hasCar = true
+hasLicense = false
+canReachPedals = true
+
+
+const meetsAllCriteria = hasLicense
+//CALCULATION
+console.log(hasLicense)
+
+//DECISION
+if(hasLicense){ //acts as a gate 
+    console.log("Zoom Zoom") // did not run??
+}
+
+hasLicense = true
+
+console.log(hasLicense)
+
+if(hasLicense){ //acts as a gate 
+    console.log("Zoom Zoom") // did not run??
+}
